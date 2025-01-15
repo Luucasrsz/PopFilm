b1973f48918a9d364a1ea0aed38dc02b97d12b14
Luucasrsz <lucasrodriguezsanchez03@gmail.com>
2025-01-15 09:26:47 +0100

    rutas

--- back-end/python/routes/ruta_peliculas.py
+++ back-end/python/routes/ruta_peliculas.py
@@ -2,44 +2,44 @@ from flask import request, session
 import json
 import decimal
 from __main__ import app
-import controlador_juegos
+from controllers import peliculas_controller
 
 class Encoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal): return float(obj)
 
-@app.route("/juegos",methods=["GET"])
-def juegos():
-    juegos,code= controlador_juegos.obtener_juegos()
-    return json.dumps(juegos, cls = Encoder),code
+@app.route("/peliculas",methods=["GET"])
+def pelicuas():
+    peliculas,code= peliculas_controller.obtener_peliculas()
+    return json.dumps(peliculas, cls = Encoder),code
 
-@app.route("/juego/<id>",methods=["GET"])
-def juego_por_id(id):
-    juego,code = controlador_juegos.obtener_juego_por_id(id)
-    return json.dumps(juego, cls = Encoder),code
+@app.route("/pelicula/<id>",methods=["GET"])
+def pelicula_por_id(id):
+    pelicula,code = peliculas_controller.obtener_pelicula_por_id(id)
+    return json.dumps(pelicula, cls = Encoder),code
 
-@app.route("/juegos",methods=["POST"])
-def guardar_juego():
+@app.route("/peliculas",methods=["POST"])
+def guardar_pelicula():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        juego_json = request.json
-        ret,code=controlador_juegos.insertar_juego(juego_json["nombre"], juego_json["descripcion"], float(juego_json["precio"]), juego_json["foto"])
+        pelicula_json = request.json
+        ret,code=peliculas_controller.insertar_pelicula(pelicula_json["nombre"], pelicula_json["sinopsis"], (pelicula_json["categoria"]), pelicula_json["portada"])
     else:
         ret={"status":"Bad request"}
         code=401
     return json.dumps(ret), code
 
-@app.route("/juegos/<id>", methods=["DELETE"])
-def eliminar_juego(id):
-    ret,code=controlador_juegos.eliminar_juego(id)
+@app.route("/peliculas/<id>", methods=["DELETE"])
+def eliminar_pelicula(id):
+    ret,code=peliculas_controller.eliminar_pelicula(id)
     return json.dumps(ret), code
 
-@app.route("/juegos", methods=["PUT"])
-def actualizar_juego():
+@app.route("/peliculas", methods=["PUT"])
+def actualizar_pelicula():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        juego_json = request.json
-        ret,code=controlador_juegos.actualizar_juego(juego_json["id"],juego_json["nombre"], juego_json["descripcion"], float(juego_json["precio"]),juego_json["foto"])
+        pelicula_json = request.json
+        ret,code=peliculas_controller.actualizar_pelicula(pelicula_json["id"],pelicula_json["nombre"], pelicula_json["sinopsis"], (pelicula_json["categoria"]),pelicula_json["portada"])
     else:
         ret={"status":"Bad request"}
         code=401
